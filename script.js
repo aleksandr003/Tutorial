b32114497a65a1611a901cc8799b545e7a3682de
aleksandr003 <herasymenko.oleks@gmail.com>
2022-07-06 18:47:11 +0300

    tests

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-const mango = {
-  username: "Mango",
-  showUsername() {
-    console.log(this);
-    console.log(`My username is: ${this.username}`);
-  },
-};
+"use strict";
+/*
+ * Події елементів форм. focus, blur, change, input, submit
+ */
 
-const btn = document.querySelector(".js-btn");
-mango.showUsername();
+const usernameInputEl = document.querySelector(".js-username-input");
+const checkboxEl = document.querySelector(".js-policy-checkbox");
+const usernameOutputEl = document.querySelector(".js-username-output");
+const submitBtnEl = document.querySelector(".js-contact-form-submit");
+const contactFormEl = document.querySelector(".js-contact-form");
 
-btn.addEventListener("click", mango.showUsername.bind(mango));
+// usernameInputEl.addEventListener('focus', () => {
+//   usernameInputEl.style.outline = '1px solid teal';
+// });
+
+// usernameInputEl.addEventListener('blur', () => {
+//   usernameInputEl.style.outline = '1px solid tomato';
+// });
+
+// usernameInputEl.addEventListener('input', () => {
+//   console.log('Hello');
+// });
+
+// usernameInputEl.addEventListener('change', () => {
+//   console.log('Hello');
+// });
+
+// checkboxEl.addEventListener('change', () => {
+//   console.log('Hello');
+// });
+
+usernameInputEl.addEventListener("input", () => {
+  usernameOutputEl.textContent = usernameInputEl.value;
+});
+
+checkboxEl.addEventListener("change", () => {
+  if (checkboxEl.checked && usernameInputEl.value !== "") {
+    submitBtnEl.disabled = false;
+  } else {
+    submitBtnEl.disabled = true;
+  }
+});
+
+contactFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // contactFormEl.reset();
+  console.log("Hello");
+});
